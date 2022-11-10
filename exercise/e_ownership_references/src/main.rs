fbe98eeaa7bc40ff9e0905070e4d6d62a8926793
Pedro Machado Leiva <pyuris22@gmail.com>
2022-11-10 09:41:09 -0500

    e exercise done

--- exercise/e_ownership_references/src/main.rs
+++ exercise/e_ownership_references/src/main.rs
@@ -14,25 +14,26 @@ fn main() {
     // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
     // String reference
     //
-    //inspect(&arg);
+    inspect(&arg);
 
     // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
     // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
-    //change(&mut arg);
-    //println!("I have many {}", arg);
+    change(&mut arg);
+    //change_dereference_manually(&mut arg);
+    println!("I have many {}", arg);
 
     // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
     // indicating whether or not the String both starts with a "b" AND contains an "a".
     // Hint 1: use `.starts_with("b")` and `.contains("a")`
     // Hint 2: `&&` is the boolean "AND" operator
     //
-    //if eat(arg) {
-    //    println!("Might be bananas");
-    //} else {
-    //    println!("Not bananas");
-    //}
+    if eat(arg) {
+        println!("Might be bananas");
+    } else {
+        println!("Not bananas");
+    }
 
     // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
 
@@ -43,8 +44,45 @@ fn main() {
     // Hint: You will need to dereference the mutable reference in order to assign it a
     // new value.
     //
-    // let mut material = "mud".to_string();
-    // println!("This material is just `{}`.", material);
-    // bedazzle(&mut material);
-    // println!("Wow! Now the material is `{}`!", material);
+    let mut material = "mud".to_string();
+    println!("This material is just `{}`.", material);
+    bedazzle(&mut material);
+    println!("Wow! Now the material is `{}`!", material);
+}
+//# 1 exercise
+fn inspect(s: &String) {
+    if s.ends_with("s") {
+        println!("plural");
+    }else {
+        println!("singular");
+    }
+}
+
+//# 2 exercise
+fn change(s: &mut String) {
+    if ! s.ends_with("s") {
+        s.push_str("s");//dereference automaticaly with dot syntaxis
+    }
+}
+
+//# 3 exercise
+fn eat(s:String) -> bool {
+    if ! s.starts_with("b") && s.contains("a") {
+        false
+    }else {
+        true
+    }
+}
+
+//# chalanger exercise
+
+fn bedazzle (s: &mut String) {
+    *s = String::from("sparkly");
+}
+
+//# my own chalanger
+fn change_dereference_manually(s: &mut String) {
+    if ! s.ends_with("s") {
+        *s = s.to_owned() + &"s".to_string();
+    }
 }
